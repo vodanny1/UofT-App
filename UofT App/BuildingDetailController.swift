c9c441afcc26950434580e0d59ba07ad5a6aec20
Danny Vo <43733358+vodanny1@users.noreply.github.com>
2020-06-27 13:56:56 -0400

    - Adding working building search function - Fixed up course detail view when there is no time slots

--- UofT App/BuildingDetailController.swift
+++ UofT App/BuildingDetailController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class BuildingDetailController: UIViewController {
+class BuildingDetailController: UIViewController, UITableViewDelegate {
+    @IBOutlet var tableView: UITableView!
     var building: BuildingResult!
+    var detail = [String]() // array containing all the information for buildings
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = building.name
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.allowsSelection = false
+        
+        detail.append("Name: " + building.name)
+        detail.append("Code: " + building.code)
+        detail.append("Short Name: " + building.short_name)
+        detail.append("Street: " + building.address.street)
+        detail.append("Campus: " + building.address.city)
     }
 
 }
+
+extension BuildingDetailController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return detail.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "buildingCell", for: indexPath)
+        
+        cell.textLabel?.numberOfLines = 0
+        
+        cell.textLabel?.text = detail[indexPath.row]
+        return cell
+    }
+}
+
+
--- UofT App/CourseDetailController.swift
+++ UofT App/CourseDetailController.swift
@@ -12,7 +12,7 @@ class CourseDetailController: UIViewController {
     @IBOutlet var tableView: UITableView!
     var course: CourseResult!
     var detail = [String]() // array containing all the information for courses
-    var total = [[String]]() // array containg both the DETAIL and the DATE/ Time
+    var total = [[String]]() // array containing both the DETAIL and the DATE/ Time
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -61,38 +61,39 @@ class CourseDetailController: UIViewController {
         while index < course.meeting_sections.count {
             var courseDate = [String]()
             courseDate.append("Section: " + course.meeting_sections[index].code.uppercased())
-            
-            if (course.meeting_sections[index].times[0].day != nil) {
-                courseDate.append("Day: " + course.meeting_sections[index].times[0].day!.capitalized)
-            } else {
-                courseDate.append("Day: Not available")
-            }
-            
-            let start = course.meeting_sections[index].times[0].start! / 3600
-            if start == 0 {
-                courseDate.append("Start: Not Available")
-            } else {
-                courseDate.append("Start: " + String(start) + ":00")
-            }
-            
-            let end = course.meeting_sections[index].times[0].end! / 3600
-            if end == 0 {
-                courseDate.append("End: Not Available")
-            } else {
-                courseDate.append("End: " + String(end) + ":00")
-            }
-            
-            let duration = course.meeting_sections[index].times[0].duration! / 3600
-            if duration == 0 {
-                courseDate.append("Duration: Not Available")
-            } else {
-                courseDate.append("Duration: " + String(duration) + ":00")
-            }
-            
-            if (course.meeting_sections[index].times[0].location != nil){
-                courseDate.append("Location: " + course.meeting_sections[index].times[0].location!)
-            } else {
-                courseDate.append("Location: Not available")
+            if !(course.meeting_sections[index].times.isEmpty) {
+                if (course.meeting_sections[index].times[0].day != nil) {
+                    courseDate.append("Day: " + course.meeting_sections[index].times[0].day!.capitalized)
+                } else {
+                    courseDate.append("Day: Not available")
+                }
+                
+                let start = course.meeting_sections[index].times[0].start! / 3600
+                if start == 0 {
+                    courseDate.append("Start: Not Available")
+                } else {
+                    courseDate.append("Start: " + String(start) + ":00")
+                }
+                
+                let end = course.meeting_sections[index].times[0].end! / 3600
+                if end == 0 {
+                    courseDate.append("End: Not Available")
+                } else {
+                    courseDate.append("End: " + String(end) + ":00")
+                }
+                
+                let duration = course.meeting_sections[index].times[0].duration! / 3600
+                if duration == 0 {
+                    courseDate.append("Duration: Not Available")
+                } else {
+                    courseDate.append("Duration: " + String(duration) + ":00")
+                }
+                
+                if (course.meeting_sections[index].times[0].location != nil){
+                    courseDate.append("Location: " + course.meeting_sections[index].times[0].location!)
+                } else {
+                    courseDate.append("Location: Not available")
+                }
             }
             
             if (course.meeting_sections[index].size != 0){
